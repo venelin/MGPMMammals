0470ed4cbfd22538085cd19fc7546d4119cb3998
Venelin Mitov <venelin.mitov@bsse.ethz.ch>
2019-01-15 11:16:21 +0100

    Preparing best-clade-2 run for the mammal MGPM fit. Moving data-raw/*.RData files to Results, which is build-ignored.

---
+++ data-raw/DetectShifts_MGPM_A_F_best_clade_2.R
@@ -0,0 +1,120 @@
+# This script can be run locally or on a cluster using a command like:
+#
+# bsub -M 400000 -n 400 -W 23:59 -R ib sh R --vanilla --slave -f ../../DetectShifts_MGPM_A_F_best_clade_2.R
+#
+library(ape)
+library(PCMBase)
+library(PCMBaseCpp)
+library(PCMFit)
+library(data.table)
+library(MGPMMammals)
+
+
+args <- commandArgs(trailingOnly = TRUE)
+if(length(args) > 0) {
+  id <- as.integer(args[1])
+} else {
+  id <- 1
+}
+
+prefixFiles = paste0("MGPM_A_F_best_clade_2_1_")
+
+if(!exists("cluster") || is.null(cluster)) {
+  if(require(doMPI)) {
+    # using MPI cluster as distributed node cluster (possibly running on a cluster)
+    # Get the number of cores. Assume this is run in a batch job.
+    p = strtoi(Sys.getenv('LSB_DJOB_NUMPROC'))
+    cluster <- startMPIcluster(count = p-1, verbose = TRUE)
+    doMPI::registerDoMPI(cluster)
+  } else {
+    cluster <- parallel::makeCluster(parallel::detectCores(logical = TRUE),
+                                     outfile = paste0("log_", prefixFiles, ".txt"))
+    doParallel::registerDoParallel(cluster)
+  }
+}
+
+tableFits <- NULL
+# try using a previously stored tableFits from a previous run that was interupted
+fileCurrentResults <- paste0("CurrentResults_", prefixFiles, ".RData")
+if(file.exists(fileCurrentResults)) {
+  cat("Loading previously stored tableFits from file", fileCurrentResults, "...\n")
+
+  # this should load a list object named listResults containg a named entry "tableFits":
+  load(fileCurrentResults)
+
+  tableFits <- listResults$tableFits
+
+  tempFiles <- list.files(pattern = paste0("^", prefixFiles, ".*.RData"))
+  if(length(tempFiles) > 0) {
+    cat("Loading previously stored tableFits from temporary files (", toString(tempFiles), ")...\n")
+    tableFitsTempFiles <- rbindlist(
+      lapply(tempFiles, function(file) {
+        load(file)
+        data
+      }))
+    tableFits <- rbindlist(list(tableFits, tableFitsTempFiles))
+  }
+
+  setkey(tableFits, hashCodeTree,hashCodeStartingNodesRegimesLabels,hashCodeMapping)
+
+  tableFits <- unique(tableFits, by = key(tableFits))
+  tableFits[, duplicated:=FALSE]
+
+  print(tableFits)
+}
+
+options(PCMBase.Value.NA = -1e20)
+options(PCMBase.Lmr.mode = 11)
+# for a few simulations there was an eigenvalue below the threshold of 1e-5 for the
+# variance covariance transition matrix on 1 node. To calculate the likelihood
+# and AIC for these 10 simulations we set the threshold to a more tolerant value.
+# In the inference, the threshold will be left as per defaul (i.e. 1e-5).
+options(PCMBase.Threshold.EV = 1e-7)
+
+print(PCMOptions())
+
+
+fitMappings <- PCMFitMixed(
+
+  X = MGPMMammals::values, MGPMMammals::tree = tree, SE = MGPMMammals::SEs,
+
+  metaIFun = PCMInfoCpp, positiveValueGuard = 5000,
+
+  tableFits = tableFits,
+
+  listPartitions = NULL,
+
+  minCladeSizes = 20L,
+
+  maxCladePartitionLevel = 100L, maxNumNodesPerCladePartition = 1L,
+
+  listAllowedModelTypesIndices = "best-clade-2",
+
+  argsConfigOptim1 = DefaultArgsConfigOptim(
+    numCallsOptim = 400L,
+    numRunifInitVecParams = 100000L,
+    numGuessInitVecParams = 50000L),
+  argsConfigOptim2 = DefaultArgsConfigOptim(
+    numCallsOptim = 10L,
+    numRunifInitVecParams = 1000L,
+    numGuessInitVecParams = 1000L,
+    numJitterRootRegimeFit = 1000L,
+    numJitterAllRegimeFits = 1000L),
+
+  doParallel = TRUE,
+
+  prefixFiles = prefixFiles,
+  saveTempWorkerResults = TRUE,
+  printFitVectorsToConsole = TRUE,
+  verbose = TRUE,
+  debug = FALSE)
+
+save(fitMappings, file = paste0("FinalResult_", prefixFiles, ".RData"))
+
+if(exists("cluster") && !is.null(cluster)) {
+  parallel::stopCluster(cluster)
+  # Don't forget to destroy the parallel cluster to avoid leaving zombie worker-processes.
+
+  cluster <- NULL
+}
+
---
+++ data-raw/DetectShifts_MGPM_A_F_best_clade_2_bsub.sh
@@ -0,0 +1,11 @@
+mkdir -p Results/MGPM_A_F_best_clade_2
+cd Results/MGPM_A_F_best_clade_2
+if [ -f "FinalResult_MGPM_A_F_best_clade_2_1_.RData" ]
+then
+rm MPI*.log
+rm CurrentResults*.RData
+else
+bsub -M 250000 -n 250 -W 23:59 -R ib sh R --vanilla --slave -f ../../DetectShifts_MGPM_A_F_best_clade_2.R
+fi
+cd ../..
+done
